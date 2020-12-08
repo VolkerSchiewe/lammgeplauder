ea1b59ea92c8fa25d14cc609f94f9081545fe954
Volker <volker@schiewe.dev>
2020-12-08 11:36:54 +0100

    Purge tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,15 +1,18 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-  purge: [],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    colors: {
-      orange:colors.orange
+    purge: [
+        './components/**/*.{js,ts,jsx,tsx}',
+        './pages/**/*.{js,ts,jsx,tsx}'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        colors: {
+            orange: colors.orange
+        },
+        extend: {},
     },
-    extend: {},
-  },
-  variants: {
-    extend: {},
-  },
-  plugins: [],
+    variants: {
+        extend: {},
+    },
+    plugins: [],
 }
