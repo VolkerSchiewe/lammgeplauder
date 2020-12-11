c4af93645afd007a556dbff070679c7973cd6c86
Volker <volker@schiewe.dev>
2020-12-11 11:28:29 +0100

    Add link to Apple Podcasts, improve responsibility

--- pages/index.tsx
+++ pages/index.tsx
@@ -8,15 +8,15 @@ const HomePage = () => (
            width={ 200 } height={ 200 }/>
     <h1 className={ "text-3xl sm:text-5xl text-center" }>Lammgeplauder Podcast</h1>
     <span>Ein Podcast der EBU-Jugend</span>
-    <div className={ "grid gap-5 grid-cols-3" }>
+    <div className={ "grid gap-x-5 grid-cols-1 md:grid-cols-3" }>
       <a href={ "" }>
-        <Image src={ "/google-podcast-badge.svg" } alt={ "Bei Google Podcast anhören" } width={ 200 } height={ 100 }/>
+        <Image src={ "/google-podcast-badge.svg" } alt={ "Bei Google Podcast anhören" } width={ 200 } height={ 50 }/>
       </a>
-      <a href={ "" }>
-        <Image src={ "/apple-podcast-badge.svg" } alt={ "Anhören auf Apple Podcast" } width={ 200 } height={ 100 }/>
+      <a href={ "https://podcasts.apple.com/de/podcast/lammgeplauder-podcast/id1544078980" }>
+        <Image src={ "/apple-podcast-badge.svg" } alt={ "Anhören auf Apple Podcast" } width={ 200 } height={ 50 }/>
       </a>
       <a href={ "https://open.spotify.com/show/7lr8TBhdwTH6EzbNBxvKXM" }>
-        <Image src={ "/spotify-podcast-badge.svg" } alt={ "Anhören auf Spotify" } width={ 200 } height={ 100 }/>
+        <Image src={ "/spotify-podcast-badge.svg" } alt={ "Anhören auf Spotify" } width={ 200 } height={ 50 }/>
       </a>
     </div>
   </Layout>
