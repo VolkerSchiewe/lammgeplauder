a877cb86fb10fcd2d3403da998e2e33d2d989eb9
Volker <volker@schiewe.dev>
2020-12-11 16:44:25 +0100

    Add notification when copying feed url

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,12 +1,16 @@
 import Layout from '../components/Layout'
-import React from "react";
+import React, { useState } from "react";
 import Image from "next/image"
 import absoluteUrl from "next-absolute-url/index";
 
 const HomePage = () => {
+  const [notificationOpen, openNotification] = useState(false)
+
   async function copyFeedToClipboard() {
     const { origin } = absoluteUrl()
     await navigator.clipboard.writeText(`${ origin }/api/feed`)
+    openNotification(true)
+    setTimeout(() => openNotification(false), 2000)
   }
 
   return (
@@ -36,6 +40,22 @@ const HomePage = () => {
                   d="M8 5H6a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2v-1M8 5a2 2 0 002 2h2a2 2 0 002-2M8 5a2 2 0 012-2h2a2 2 0 012 2m0 0h2a2 2 0 012 2v3m2 4H10m0 0l3-3m-3 3l3 3"/>
           </svg>
           { "Feed - URL kopieren" }
+        </div>
+      </div>
+      <div
+        className={ `absolute w-full flex justify-end items-start p-5 ${ notificationOpen ? "opacity-100" : "opacity-0" } transition-opacity duration-500 ease-out ` }>
+        <div className=" bg-white rounded-lg border-gray-300 border p-3 shadow-lg">
+          <div className="flex flex-row">
+            <div className="px-2">
+              <svg width="24" height="24" viewBox="0 0 1792 1792" fill="#44C997" xmlns="http://www.w3.org/2000/svg">
+                <path
+                  d="M1299 813l-422 422q-19 19-45 19t-45-19l-294-294q-19-19-19-45t19-45l102-102q19-19 45-19t45 19l147 147 275-275q19-19 45-19t45 19l102 102q19 19 19 45t-19 45zm141 83q0-148-73-273t-198-198-273-73-273 73-198 198-73 273 73 273 198 198 273 73 273-73 198-198 73-273zm224 0q0 209-103 385.5t-279.5 279.5-385.5 103-385.5-103-279.5-279.5-103-385.5 103-385.5 279.5-279.5 385.5-103 385.5 103 279.5 279.5 103 385.5z"/>
+              </svg>
+            </div>
+            <div className="ml-2 mr-6">
+              <span className="font-semibold">Feed-Url kopiert!</span>
+            </div>
+          </div>
         </div>
       </div>
     </Layout>
