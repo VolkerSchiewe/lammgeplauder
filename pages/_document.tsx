b457d73a748b0f45718b4f208732c28b34bf0cc5
Volker <volker@schiewe.dev>
2021-01-05 10:07:42 +0100

    Add improved favicons and app icons

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -8,6 +8,12 @@ class MyDocument extends Document {
         <Head>
           <meta name="description"
                 content={ "Lammgeplauder ist ein Podcast der Jugend der evangelischen Brüder-Unität - Herrnhuter Brüdergemeine. Wir wollen gemeinsam Themen, wie Nachhaltigkeit, Rechtsextremismus, Mission in der EBU heutzutage und viele andere Themen, die uns in der Jugend beschäftigen nach außen bringen, sodass alle Gemeindemitglieder, egal welchen Alters, in der Lage sind, sich auch mit diesen Themen auseinander zu setzen." }/>
+          <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png"/>
+          <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png"/>
+          <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png"/>
+          <link rel="manifest" href="/manifest.json"/>
+          <meta name="msapplication-TileColor" content="#ffffff"/>
+          <meta name="theme-color" content="#ffffff"/>
         </Head>
         <body>
         <Main/>
