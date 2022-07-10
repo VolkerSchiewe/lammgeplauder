02fd0fb152e69dae61e8ba573db2b956b18b4e86
Volker <volker@schiewe.dev>
2022-07-10 22:14:29 +0200

    Fix cypress config

--- cypress.config.ts
+++ cypress.config.ts
@@ -3,7 +3,6 @@ import { defineConfig } from 'cypress'
 export default defineConfig({
   fixturesFolder: false,
   e2e: {
-    setupNodeEvents(on, config) {},
     baseUrl: 'http://localhost:3000',
   },
 })
