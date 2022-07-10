1ab368d122e2f211b548f297a0e11c2b0eb20fa2
Volker <volker@schiewe.dev>
2022-07-10 22:00:29 +0200

    Upgrade tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,20 +1,15 @@
 const colors = require('tailwindcss/colors')
 module.exports = {
-    mode: 'jit',
-    purge: [
+    content: [
         './components/**/*.{js,ts,jsx,tsx}',
         './pages/**/*.{js,ts,jsx,tsx}'
     ],
-    darkMode: false, // or 'media' or 'class'
     theme: {
         extend: {
             colors: {
                 orange: colors.orange
             },
         },
-    },
-    variants: {
-        extend: {},
     },
     plugins: [
         require('@tailwindcss/forms'),
