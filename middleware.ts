22eabf2a49ac932582d4057c974bf7a65a96ab7a
Volker <volker@schiewe.dev>
2024-07-02 17:56:48 +0200

    exclude feed api from auth middleware

--- middleware.ts
+++ middleware.ts
@@ -2,8 +2,13 @@ import { type NextRequest, NextResponse } from "next/server";
 import { SESSION_COOKIE_NAME, ROOT_ROUTE } from "./constants";
 
 export const config = {
-  matcher: ['/api/:path*', '/admin/:path*'],
-}
+  matcher: [
+    "/api/podcast",
+    "/api/episodes",
+    "/api/episodes/:path*",
+    "/admin/:path*",
+  ],
+};
 export default async function middleware(request: NextRequest) {
   const session = request.cookies.get(SESSION_COOKIE_NAME)?.value || "";
   if (!session) {
